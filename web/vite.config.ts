d080c77d2583dceeaaf6c1e0de1c8af0badbeaa3
sat engineer <sovereigncircuits@protonmail.com>
2025-11-15 16:55:36 -0500

    ci tweaks

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
     },
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'json', 'html', 'lcov'],
+      reporter: ['text', 'json', 'json-summary', 'html', 'lcov'],
       exclude: ['node_modules/', 'src/test/', '**/*.d.ts', 'dist/', 'coverage/', '**/*.config.*'],
       thresholds: {
         global: {
